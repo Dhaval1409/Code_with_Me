229541beafd0fbe851636ad7d20437f97ac6e019
Dhaval1409 <bodardhaval86@gmail.com>
2025-11-11 17:11:34 +0530

    learning about structure of next js folder

---
+++ Day_4/my-app/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello this is day 4 of learning consistency with meeeeeeee.
+    </div>
+  )
+}
+
+export default page
